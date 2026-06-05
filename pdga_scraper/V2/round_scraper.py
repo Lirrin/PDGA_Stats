704a7a173880c63de4eec4947d413e6218e1f27a
Lirrin <rssolmyr@gmail.com>
2026-06-05 12:30:45 -0400

    Updated to get round stats and hole breakdown stats

--- pdga_scraper/V2/round_scraper.py
+++ pdga_scraper/V2/round_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 def get_round(event_id, division, round_number):
     url = "https://www.pdga.com/apps/tournament/live-api/live_results_fetch_round"
@@ -10,6 +11,14 @@ def get_round(event_id, division, round_number):
     }
     
     return requests.get(url, params=params).json()
+
+def get_hole_breakdown(score_id):
+    url = f"https://www.pdga.com/api/v1/feat/live-scores/{score_id}/hole-breakdowns"  
+    return requests.get(url).json()
+
+def get_round_stats(score_id):
+    url = f"https://www.pdga.com/api/v1/feat/live-scores/{score_id}/round-stats"  
+    return requests.get(url).json()
 
 def parse_round(event_id: int, division: str, round_number: int, payload: dict):
     data = payload["data"]
@@ -30,20 +39,54 @@ def parse_round(event_id: int, division: str, round_number: int, payload: dict):
     round_context = []
     hole_scores = []
     players = []
+    round_context_stats = []
     for player_round in data["scores"]:
         pars = player_round.get("Pars", "").split(",")
+        score_id = player_round["ScoreID"]
+
+        hole_breakdowns = get_hole_breakdown(score_id=score_id)
+        round_stats = get_round_stats(score_id=score_id)
+
+        breakdown_by_hole = {
+            h["holeOrdinal"]: h
+            for h in hole_breakdowns
+        }
+
         for i, score in enumerate(player_round.get("HoleScores", [])):
+            breakdown = breakdown_by_hole.get(i+1, None)
+
+            hole_score = int(score) if score != "" else None
+            par = int(pars[i]) if i < len(pars) and pars[i] != "" else None
+
+            score_to_par = (
+                hole_score - par
+                if hole_score is not None and par is not None
+                else None
+            )
 
             hole_scores.append({
                 "result_id": player_round["ResultID"], # unsure the difference between result id and score id
                 "round_id": player_round["RoundID"], #ties to live round ID at round info level, so theoretically don't need event id
-                "score_id": player_round["ScoreID"], # unsure the difference between result id and score id
+                "score_id": score_id, # score id ties to the stats for the hole score and round
                 "round_number": round_number,
                 "pdga_number": player_round["PDGANum"],
                 "hole_number": i+1,
-                "score": int(score) if score != "" else None,
-                "par": int(pars[i]) if i < len(pars) else None,
-                "score_to_par": int(score) - int(pars[i]) if score != "" and i < len(pars) else None
+                "score": hole_score,
+                "par": par,
+                "score_to_par": score_to_par,
+                #additional breakdown info we can pull in if we want
+                "driving": breakdown.get("driving"),
+                "scramble": breakdown.get("scramble"),
+                "green": breakdown.get("green"),
+                "c1x": breakdown.get("c1x"),
+                "c1": breakdown.get("c1"),
+                "c2": breakdown.get("c2"),
+                "throwIn": breakdown.get("throwIn"),
+                "ob": breakdown.get("ob"),
+                "hazard": breakdown.get("hazard"),
+                "missedMando": breakdown.get("missedMando"),
+                "lostDisc": breakdown.get("lostDisc"),
+                "penalty": breakdown.get("penalty")
             })
         round_context.append({
             # IDs
@@ -89,15 +132,23 @@ def parse_round(event_id: int, division: str, round_number: int, payload: dict):
             "home_country": player_round["Country"],
             "full_location": player_round["FullLocation"]
         })
+        for stat in round_stats:
+            round_context_stats.append({
+                "score_id": score_id,
+                "stat_id": stat["statId"],
+                "stat_count": stat["statCount"],
+                "stat_opportunity_count:": stat["statOpportunityCount"],
+                "stat_value": stat["statValue"]
+            })
+        
+        time.sleep(0.5) # to avoid hitting api rate limits 
 
-
-
-    return round_info, hole_scores, round_context, players
+    return round_info, hole_scores, round_context, players, round_context_stats
 
 
 payload = get_round(96410, "MPO", 1)
 
-round_info, hole_scores, round_context, players = parse_round(96410, "MPO", 1, payload)
+round_info, hole_scores, round_context, players, round_stats = parse_round(96410, "MPO", 1, payload)
 print(round_info)
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print(hole_scores[0])
@@ -105,3 +156,5 @@ print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print(round_context[0])
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print(players[0])
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print(round_stats[0])
